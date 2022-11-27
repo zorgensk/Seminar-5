500db503cc9c76b4e7239499e86287b47c920121
zorgensk <zorgensk@gmail.com>
2022-11-27 15:57:51 +0700

    HomeWork3

---
+++ HomeWork_3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+void PrintArray(int[] arr) // Метод вывода массива в терминал
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"[{arr[i]}]");
+    }
+    Console.WriteLine(" ");
+}
+
+void RandomArray(int[] arr) // Метод заполнения массива случайными числами
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(-10, 11);
+    }
+}
+
+int []arr = new int[5];
+
+RandomArray(arr);
+PrintArray(arr);
+
+Console.WriteLine($"Разница между максимальным и минимальным элементов массива: {arr.Max() - arr.Min()}");
